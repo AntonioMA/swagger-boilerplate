e323b5a023af7ced0ebc0de269300619a1fe64d6
Antonio M. Amaya <amac@tid.es>
2017-04-06 09:34:16 +0200

    It's utils not Utils

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 var Server = require('./lib/server');
 var ServerPersistence = require('./lib/serverPersistence');
 var JSONPersistenceProvider = require('./lib/jsonPersistenceProvider');
-var Utils = require('./lib/shared/Utils');
+var Utils = require('./lib/shared/utils');
 var ErrorInfo = require('./lib/error_info');
 var App = require('./lib/app.js');
 module.exports = {};
