ba6e9c359b83cad3ceb18102b1d04f9f14198368
Antonio M. Amaya <amac@tid.es>
2018-03-08 16:48:53 +0100

    [IE-704] Add RestAPI wrapper here

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ var JSONPersistenceProvider = require('./lib/jsonPersistenceProvider');
 var Utils = require('./lib/shared/utils');
 var ErrorInfo = require('./lib/error_info');
 var App = require('./lib/app.js');
-module.exports = {};
-
-module.exports.Server = Server;
-module.exports.JSONPersistenceProvider = JSONPersistenceProvider;
-module.exports.ServerPersistence = ServerPersistence;
-module.exports.Utils = Utils;
-module.exports.ErrorInfo = ErrorInfo;
-module.exports.App = App;
+var RestAPI = require('./lib/restAPI');
+module.exports = {
+  Server,
+  JSONPersistenceProvider,
+  ServerPersistence,
+  Utils,
+  ErrorInfo,
+  App,
+  RestAPI,
+};
